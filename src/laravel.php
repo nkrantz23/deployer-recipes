7f2e3c03732e53a137ca030c827e2ae768b56864
Cory Crowther <cory@petflow.com>
2018-01-31 17:04:25 -0500

    fix: add reload

--- src/laravel.php
+++ src/laravel.php
@@ -19,6 +19,12 @@ task('php-fpm:restart', function () {
     }
 })->desc('Restart php-fpm to clear opcache');
 
+task('php-fpm:reload', function () {
+    if (get('reload_fpm')) {
+        run('sudo service php7.1-fpm reload');
+    }
+})->desc('Reload php-fpm to clear opcache');
+
 task('artisan:config:clear', function () {
     run('{{bin/php}} {{release_path}}/artisan config:clear');
 })->desc('Clear the configuration cache');
